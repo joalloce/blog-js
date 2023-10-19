a577c7aee5a6007eb2cb88d781980d50579d3d8f
Jose Lo <joalloce@gmail.com>
2023-10-19 09:57:36 -0600

    feat(comment): crud comments

--- backend/src/test/commentRoutes.test.js
+++ backend/src/test/commentRoutes.test.js
@@ -13,13 +13,37 @@ afterAll(async () => {
 });
 
 describe("Comment API", () => {
+  let commentId;
+
+  it("should create a new comment", async () => {
+    const res = await request(server).post("/api/comments/").send({
+      articleId: 1,
+      author: 1,
+      content: "content",
+    });
+    commentId = res.body.data.id;
+    expect(res.statusCode).toEqual(201);
+  });
+
   it("should show all comments", async () => {
     const res = await request(server).get("/api/comments");
     expect(res.statusCode).toEqual(200);
   });
 
   it("should show a comment", async () => {
-    const res = await request(server).get("/api/comments/1");
+    const res = await request(server).get(`/api/comments/${commentId}`);
     expect(res.statusCode).toEqual(200);
+  });
+
+  it("should update a comment", async () => {
+    const res = await request(server).patch(`/api/comments/${commentId}`).send({
+      content: "content1",
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it("should delete a comment", async () => {
+    const res = await request(server).delete(`/api/comments/${commentId}`);
+    expect(res.statusCode).toEqual(204);
   });
 });
