9f25c7664aa2b3a28d7041ae7a5952fa2c32849c
Jose Lo <joalloce@gmail.com>
2023-10-19 09:52:57 -0600

    feat(article): crud tests

--- backend/src/test/articleRoutes.test.js
+++ backend/src/test/articleRoutes.test.js
@@ -13,13 +13,38 @@ afterAll(async () => {
 });
 
 describe("Article API", () => {
+  let articleId;
+
+  it("should create a new article", async () => {
+    const res = await request(server).post("/api/articles/").send({
+      title: "title",
+      content: "content",
+      author: 1,
+    });
+    articleId = res.body.data.id;
+    expect(res.statusCode).toEqual(201);
+  });
+
   it("should show all articles", async () => {
-    const res = await request(server).get("/api/users");
+    const res = await request(server).get("/api/articles");
     expect(res.statusCode).toEqual(200);
   });
 
-  it("should show an articles", async () => {
-    const res = await request(server).get("/api/users/1");
+  it("should show an article", async () => {
+    const res = await request(server).get(`/api/articles/${articleId}`);
     expect(res.statusCode).toEqual(200);
+  });
+
+  it("should update an article", async () => {
+    const res = await request(server).patch(`/api/articles/${articleId}`).send({
+      title: "title1",
+      content: "content1",
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it("should delete an article", async () => {
+    const res = await request(server).delete(`/api/articles/${articleId}`);
+    expect(res.statusCode).toEqual(204);
   });
 });
