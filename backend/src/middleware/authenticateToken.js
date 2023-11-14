bb51fc1b00f1e149417fda7d117d754d446b6aef
Jose Lo <joalloce@gmail.com>
2023-11-14 01:16:38 -0600

    feat(jwt): handle token

--- backend/src/middleware/authenticateToken.js
+++ backend/src/middleware/authenticateToken.js
@@ -1,10 +1,14 @@
 import jwt from "jsonwebtoken";
 
+import accessEnv from "#root/helpers/accessEnv";
+
+const JWT_SECRET = accessEnv("JWT_SECRET", "jwtsecret");
+
 const authenticateToken = (req, res, next) => {
   const token = req.header("Authorization");
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, "your-secret-key", (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
--- backend/src/server/controllers/authController.js
+++ backend/src/server/controllers/authController.js
@@ -1,2 +1,7 @@
 import { User, Article } from "#root/db/models";
+
 import passwordCompareSync from "#root/helpers/passwordCompareSync";
+
+import accessEnv from "#root/helpers/accessEnv";
+
+const JWT_SECRET = accessEnv("JWT_SECRET", "jwtsecret");
